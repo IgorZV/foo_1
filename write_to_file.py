34595f673e31b4f6ad0dad33074b849ef018bae9
toller <toller02@hotmail.com>
2018-01-04 20:55:55 -0800

    Fix/n and adddef plus

--- write_to_file.py
+++ write_to_file.py
@@ -1,8 +1,12 @@
 file = open("testfile.txt", "w")
 
-file.writelines(["Hello Word"/n, "This is new text file"/n, "This is another line"/n, "Why? Because we can./n"])
+file.writelines(["Hello Word", "This is new text file", "This is another line", "Why? Because we can"])
 
 file.close()
 
 def plus (a,b):
-    return a+b
+    result = a+b
+    print(result)
+
+
+plus(3,4)
